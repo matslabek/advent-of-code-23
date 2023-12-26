0d769ea2041c44d090a501ef7b96e73b5adcdbb3
mateusz.slabek <mateusz.slabek@datto.com>
2023-12-26 20:31:45 +0100

    Day 8 Part 1

--- 8/haunted_wasteland.go
+++ 8/haunted_wasteland.go
@@ -14,15 +14,39 @@ func Wasteland() {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	//instructions := inputStrings[0]
+	instructions := inputStrings[0]
+	l := len(instructions)
+
 	n := len(inputStrings)
 	m := make(map[string][]string)
 
 	for i := 2; i < n; i++ {
 		s := strings.Fields(inputStrings[i])
-		mm := make([]string, 2)
+		mm := make([]string, 0)
 		mm = append(mm, s[2][1:4], s[3][0:3])
 		m[s[0]] = mm
 	}
-	fmt.Println(m)
+	// Starting point should be AAA
+	node := "AAA"
+
+	counter := 1
+	for i := 0; i < l; i++ {
+		direction := instructions[i]
+		v, _ := m[node]
+		if direction == 'R' {
+			node = v[1]
+		} else if direction == 'L' {
+			node = v[0]
+		}
+
+		if node == "ZZZ" {
+			break
+		}
+		// If we reach the end of instruction strings go back to the beginning
+		if i == l-1 {
+			i = -1
+		}
+		counter++
+	}
+	fmt.Println(counter)
 }
